f3ee747cb0457036df9a5442cdc78344409f45fb
David Fridley <davidfridley@enciv.org>
2021-12-27 15:17:27 -0800

    comments and factored out dispatch and methodState from addCommonMethods

--- use-methods.js
+++ use-methods.js
@@ -1,12 +1,19 @@
 import React, { useState, useReducer, useEffect, useCallback } from 'react'
 import { cloneDeep, isEqual } from 'lodash'
 
+
+// const [state,methods]=useMethodHistory(methodsObject, umsKey, initialState, deps)
+//
+// usmKeys need to be drilled down through components that lie between component that involve useMethods
+// if you don't pass a umsKey - one will be assigned - but if the user backs past that state, it won't be restoreable when going forward - and neither will the children
+// !!! need to do work to delete the children in that case
+
 // while "object" state  (a pointer for you C programmers) will change each time, the object state.methodState (meaning the pointer methodState) will always be the same
-// this reducer will mutate the contents of the methodState object based on the properties in the object "action'.  So action does not have a type like it would in other cases.
+// this reducer will mutate the contents of the methodState object based on the properties in the object "action'.  So action does not have a type like it would in usual cases.
 // reducer will retun a new state but with the same methodState object - though it will be mutated 
 function reducer(state, action) {
     Object.assign(state.methodState, action)
-    // after react settles, push the current state
+    // after react settles, push the current state onto history
     if(!useMethods.inPopState) {
         if(useMethods.timeout)
             clearTimeout(useMethods.timeout)
@@ -17,73 +24,78 @@ function reducer(state, action) {
     return { ...state }
 }
 
-// <Component {...methods.keys(key)} ... > will get you the key for your component and the usmKey for the child
-// usmKeys need to be drilled down through components that lie between component that involve useMethods
-// if you don't pass a umsKey - one will be assigned - but if the user backs past that state, it won't be restoreable when going forward - and neither will the children
-// !!! need to do work to delete the children in that case
+// this wrapper adds some common functions to methods, it's an external function so that it only needs to be called when deps changs.
+
+// these methods (the code) are setup once. They will always refer to the original "state" pointer that was returned by the very first useReducer call.
+// but because our "state" has "methodState" in it, and our reducer always mutates that object (rather then setting methodState= it uses objectAssign(methodState))
+// these "memorized" methods will be able to access the latest *methodState* as it changes
+// memorizing these methods saves times and reduces rerendering of components
+// writing code with methods is less work and less error prone than doing dispatch({type: "xxx"}) everywhere
+
+const addCommonMethods=(_this, methodsObj, initialState)=>{
+    let methods=methodsObj(_this.dispatch, _this.methodState)
+    methods.reset = function () {
+        // reset the methodState back to initialState by mutating the original object.  
+        // don't create a new object, becasue the methosObjs were instantiated to work with the original state.methodState object.
+        Object.keys(methodState).forEach(key => { delete methodState[key] })
+        Object.assign(methodState, initialState)
+        dispatch({})
+    }
+
+    // <Component {...methods.keys(key)} ... > will get you the key for your component and the usmKey for the child
+    methods.keys=function(key){
+        return {key, umsKey: _this.umsKey ? _this.umsKey+'.'+key : undefined}
+    }
+    return methods
+}
 
 
-export function useMethods(methodsObj, initialState, umsKey, deps) {
+export function useMethods(methodsObj, initialState, umsKey, deps=[]) {
 
     // the thing about the umsKey is that it needs to be unique accross the application running on the browser
     // but it should be the same if the user goes backward in history and then forward again - this is how we will find the state and reuse that state when the user moves forward after a component was unmounted
     // umsKey is optional but then you won't get the go forward capability
-    // umsKey should probably be something like parent-ums-key-child-key or grandparent-parent-child-key
-
-    let startState=initialState
-    const [_this]=useState({umsKey, dispatch: undefined, unmounted: false, methodState: undefined})
-    if(!_this.umsKey) {
-        if(umsKey) _this.umsKey=umsKey
-        else if(!useMethods.children.length) _this.umsKey='ums-top'
-        else _this.umsKey='ums-'+useMethods.nextUniqueUmsKey++
-    }
-    if(umsKey && !_this.dispatch){ // if no umsKey no need for this to find previous state by that key
-        for(const child of useMethods.children){
-            if(child.umsKey===umsKey){
-                if(child.unmounted){
-                    startState=cloneDeep(child.methodState)
-                    useMethods.children.delete(child)
-                    break
-                }
-                else {
-                    console.error("useMethods umsKey:",umsKey,"new call but child exists with same key" )
+   
+    const [_this]=useState(()=>{
+        // stuff in here will only run then the Component is run the first time
+        // we want as much as we can in here so we don't run it every time the parent of this rerenders
+        let startState=initialState
+        // if there is a umsKey and we find a match in children, get the current state from that child
+        if(umsKey) {
+            for(const child of useMethods.children){
+                if(child.umsKey===umsKey){
+                    if(child.unmounted){
+                        startState=child.methodState
+                        useMethods.children.delete(child)
+                        break
+                    }
+                    else {
+                        console.error("useMethods umsKey:",umsKey,"new call but child exists with same key" )
+                    }
                 }
             }
         }
-    }
+        return {
+            umsKey: umsKey || (!useMethods.children.length? 'ums-top' : 'ums-'+useMethods.nextUniqueUmsKey++),
+            methodState: cloneDeep(startState),
+            dispatch: undefined,
+            unmounted: false
+        }
+    }) // never set _this
 
+    const [state, dispatch] = useReducer(reducer, { methodState: _this.methodState })
     // useReducer returns a new pointer to "state" every time state changes, but it always returns the same value for dispatch
-    const [state, dispatch] = useReducer(reducer, { methodState: startState })
 
-    // these methods (the code) are setup once. They will always refer to the original "state" pointer that was returned by the very first useReducer call.
-    // but because our "state" has "methodState" in it, and our reducer always mutates that object (rather then setting methodState= it uses objectAssign(methodState))
-    // these "memorized" methods will be able to access the latest *methodState* as it changes
-    // memorizing these methods saves times and reduces rerendering of components
-    // writing code with methods is less work and less error prone than doing dispatch({type: "xxx"}) everywhere
-
-    const methodState = state.methodState // now you don't have to say state.methodState everywhere
-
-    const methods = useCallback(methodsObj(dispatch, methodState),deps) // dispatch and methodState aren't in deps because they never change
-
-    methods.reset = function () {
-        // reset the methodState back to initialState by mutating the original object.  
-        // don't create a new object, becasue the methosObjs were instantiated to work with the original state.methodState object.
-        Object.keys(state.methodState).forEach(key => { delete state.methodState[key] })
-        Object.assign(state.methodState, initialState)
-        dispatch({})
-    }
-
-    methods.keys=function(key){
-        return {key, umsKey: _this.umsKey ? _this.umsKey+'.'+key : undefined}
-    }
-
-    // methodsObj is a function that returns an object of methods - because we need to pass dispatch to it.  Passing methodState doesn't hurt
+    if(state.methodState !== _this.methodState) throw Error("useMethods state.methodState was not identical to _this.methodState",state,_this, "Probably React has changed")
 
     if(!_this.dispatch){
         _this.dispatch=dispatch
-        _this.methodState=methodState
         useMethods.children.add(_this)
     }
+
+    // methodsObj is a function that returns an object of methods - because we need to pass dispatch to it.  Passing methodState doesn't hurt
+    // wrapMethods puts in a few common methods
+    const methods = useCallback(addCommonMethods(_this, methodsObj, initialState ),deps) // dispatch and methodState aren't in deps because they never change
 
     useEffect(()=>{
         if(history.state.key!=='useMethods') { // this is the first time through
@@ -99,7 +111,7 @@ export function useMethods(methodsObj, initialState, umsKey, deps) {
         }
     },[])
 
-    return [methodState, methods]
+    return [_this.methodState, methods]
 }
 
 useMethods.children=new Set()
